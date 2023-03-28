5efcf538238ae88bb0890086f69f39bbf8db3141
Yu Ying Li Wen <usbthekey@gmail.com>
2023-03-28 04:28:07 -0400

    Cleaned SceneObject

--- src/SceneObject.h
+++ src/SceneObject.h
@@ -10,6 +10,7 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+
 class SceneObject
 {
 private:
@@ -19,8 +20,21 @@ private:
 public: 
 	SceneObject(); // Empty Scene Object
 
-	SceneObject(std::vector<float>* vertexes, std::vector<unsigned int>* indexes);
-	SceneObject(std::vector<glm::vec3>* vertexes, std::vector<unsigned int>* indexes);
+	template <typename T> SceneObject(std::vector<T>* vertexes, std::vector<unsigned int>* indexes)
+		:ebo_count(indexes->size())
+	{
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+		int i = 0;
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, vertexes->size() * sizeof(T), &(*vertexes)[0], GL_STATIC_DRAW);
+
+		glGenBuffers(1, &ebo);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexes->size() * sizeof(unsigned int), &(*indexes)[0], GL_STATIC_DRAW);
+	}
+
 	SceneObject(std::string obj_file);
 
 	void Bind() const;
